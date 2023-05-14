04a927cb5ac7bab381a497587d626e9139666660
William Diaz <wrdiaz@unicauca.edu.co>
2023-05-14 11:05:31 -0500

    commons agregados

---
+++ OpenMarket-Client/src/main/java/com/unicauca/edu/co/openmarket/client/access/CategoryRepositoryImplArrays.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.unicauca.edu.co.openmarket.client.access;
+
+/**
+ *
+ * @author Personal
+ */
+public class CategoryRepositoryImplArrays {
+    
+}
