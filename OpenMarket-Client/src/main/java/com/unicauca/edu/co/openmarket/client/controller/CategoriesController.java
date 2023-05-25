63dc50804df3ca2983f39ce7abfc9c933ec7a790
AlejFernandezV <alejfernandez@unicauca.edu.co>
2023-05-25 17:22:44 -0500

    Agregadas las clases Controller para category y product

---
+++ OpenMarket-Client/src/main/java/com/unicauca/edu/co/openmarket/client/controller/CategoriesController.java
@@ -0,0 +1,65 @@
+
+package com.unicauca.edu.co.openmarket.client.controller;
+
+import com.unicauca.edu.co.openmarket.client.access.CategoryAccessImplSockets;
+import com.unicauca.edu.co.openmarket.client.commands.OMAddCategoryCommand;
+import com.unicauca.edu.co.openmarket.client.commands.OMAddProductCommand;
+import com.unicauca.edu.co.openmarket.client.commands.OMInvoker;
+import com.unicauca.edu.co.openmarket.commons.domain.Category;
+import com.unicauca.edu.co.openmarket.commons.domain.Product;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class CategoriesController {
+    private CategoryAccessImplSockets categoryAccess;
+    private OMInvoker ominvoker;
+
+    public CategoriesController(CategoryAccessImplSockets categoryAccess) {
+        this.categoryAccess = categoryAccess;
+        this.ominvoker = new OMInvoker();
+    }
+    
+    public CategoryAccessImplSockets getProductService() {
+        return categoryAccess;
+    }
+
+    public void setProductService(CategoryAccessImplSockets categoryAccess) {
+        this.categoryAccess = categoryAccess;
+    }
+
+    public OMInvoker getOminvoker() {
+        return ominvoker;
+    }
+
+    public void setOminvoker(OMInvoker ominvoker) {
+        this.ominvoker = ominvoker;
+    }
+
+    public boolean addCategory(String name, String description) {
+        Category category = new Category(0L,name);
+        OMAddCategoryCommand comm = new OMAddCategoryCommand(category, categoryAccess);
+        ominvoker.addCommand(comm);
+        ominvoker.execute(); 
+        
+        return comm.result();
+    }
+
+    public boolean editProduct(String id, String name) throws Exception {
+        if (id.equals("")) {
+            return false;
+        }
+        
+        Long categoryId = Long.parseLong(id);
+        Category category = new Category();
+        category.setName(name);
+
+        return categoryAccess.editC(categoryId, category);
+    }
+    
+    public boolean deleteProduct(String id) throws Exception{
+        Long categoryId = Long.parseLong(id);
+        return categoryAccess.deleteC(categoryId);
+    }
+}
---
+++ OpenMarket-Client/src/main/java/com/unicauca/edu/co/openmarket/client/controller/ProductsController.java
@@ -0,0 +1,65 @@
+
+package com.unicauca.edu.co.openmarket.client.controller;
+
+import com.unicauca.edu.co.openmarket.client.access.ProductAccessImplSockets;
+import com.unicauca.edu.co.openmarket.client.commands.OMAddProductCommand;
+import com.unicauca.edu.co.openmarket.client.commands.OMInvoker;
+import com.unicauca.edu.co.openmarket.client.domain.services.ProductService;
+import com.unicauca.edu.co.openmarket.commons.domain.Product;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class ProductsController {
+    private ProductAccessImplSockets productAccess;
+    private OMInvoker ominvoker;
+
+    public ProductsController(ProductAccessImplSockets productService) {
+        this.productAccess = productService;
+        this.ominvoker = new OMInvoker();
+    }
+    
+    public ProductAccessImplSockets getProductService() {
+        return productAccess;
+    }
+
+    public void setProductService(ProductAccessImplSockets productService) {
+        this.productAccess = productService;
+    }
+
+    public OMInvoker getOminvoker() {
+        return ominvoker;
+    }
+
+    public void setOminvoker(OMInvoker ominvoker) {
+        this.ominvoker = ominvoker;
+    }
+
+    public boolean addProduct(String name, String description) {
+        Product product = new Product(0L,name, description, 0);
+        OMAddProductCommand comm = new OMAddProductCommand(product, productAccess);
+        ominvoker.addCommand(comm);
+        ominvoker.execute(); 
+        
+        return comm.result();
+    }
+
+    public boolean editProduct(String id, String name, String description) {
+        if (id.equals("")) {
+            return false;
+        }
+        
+        Long productId = Long.parseLong(id);
+        Product prod = new Product();
+        prod.setName(name);
+        prod.setDescription(description);
+
+        return productAccess.edit(productId, prod);
+    }
+    
+    public boolean deleteProduct(String id) throws Exception{
+        Long productId = Long.parseLong(id);
+        return productAccess.delete(productId);
+    }
+}
